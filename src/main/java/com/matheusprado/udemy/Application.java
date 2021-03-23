1af167a95132b7595a9e5be7952fee96def5f367
MatheusPMC <matheus.pmc@hotmail.com>
2021-03-23 19:27:49 -0300

    Open API and Swagger

--- src/main/java/com/matheusprado/udemy/Application.java
+++ src/main/java/com/matheusprado/udemy/Application.java
@@ -2,9 +2,20 @@ package com.matheusprado.udemy;
 
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "mn-stock-broker",
+                version = "0.1",
+                description = "Udemy Micronaut Course",
+                license = @License(name = "MIT")
+        )
+)
 public class Application {
 
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
@@ -13,7 +24,5 @@ public class Application {
        final ApplicationContext context = Micronaut.run(Application.class, args);
        final HelloWorldService service = context.getBean(HelloWorldService.class);
        LOG.info(service.sayHi());
-       context.close();
-
     }
 }
--- src/main/java/com/matheusprado/udemy/broker/MarketsController.java
+++ src/main/java/com/matheusprado/udemy/broker/MarketsController.java
@@ -2,10 +2,14 @@ package com.matheusprado.udemy.broker;
 
 import com.matheusprado.udemy.broker.model.Symbol;
 import com.matheusprado.udemy.broker.store.InMemoryStore;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller("/markets")
@@ -17,6 +21,11 @@ public class MarketsController {
         this.store = store;
     }
 
+    @Operation(summary = "Returns all available markets")
+    @ApiResponse(
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    @Tag(name = "markets")
     @Get("/")
     public List<Symbol> all() {
         return store.getAllSymbols();
